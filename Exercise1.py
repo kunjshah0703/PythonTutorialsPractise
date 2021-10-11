b9e911ea93293cb6cd7cec3384f7eecd07f00d33
kunjshah0703 <72154374+kunjshah0703@users.noreply.github.com>
2021-10-11 10:26:36 +0530

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,14 @@
+"""
+# Creating a dictionary
+
+print("Enter a word to know it's meaning: ")
+a = input( )
+dict1 = {"Awesomesauce":"Excellent",
+         "Chillax":"Calm down",
+         "Crunk":"Very excited",
+         "Ginger":"A person with red hair",
+         "Totes":"Completely"}
+print("Meaning of the Entered word is: ", dict1[a])
+"""
+
+
