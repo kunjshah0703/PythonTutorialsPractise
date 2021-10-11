467ddda9cdd5ac45514b3a17a05d53f9ceca34cc
kunjshah0703 <72154374+kunjshah0703@users.noreply.github.com>
2021-10-11 10:28:02 +0530

    Add files via upload

---
+++ Excersie2.py
@@ -0,0 +1,18 @@
+"""
+# Faulty Calculator
+
+# operator1 = input("Enter a operation to do")
+# a = 0
+# b = 0
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+operator1 = input("Enter a operation to do : ")
+if operator1 == "+":
+    print("Result is : ", 0)
+elif operator1 == "/":
+    print("Result is :", 0)
+elif operator1 == "*":
+    print("Result is :", 0)
+elif operator1 == "-":
+    print("Result is :", a - b)
+    """
